85ab54de7009a1642ceee05bc8519ffaa747ba1c
Eric Mountain <eric.stephen.mountain@gmail.com>
2016-04-03 23:25:47 +0200

    Implements proper overlap detection.

--- findtiles.py
+++ findtiles.py
@@ -9,23 +9,12 @@ import re
 # Same format as areas.list
 box = (55.560, -6.899, 57.757, -2.219)
 
-def checkInsideArea(area, coords):
-    if (coords[0] >= area[0] and
-        coords[0] <= area[2] and
-        coords[1] >= area[1] and
-        coords[1] <= area[3]):
-        return True
-    else:
-        return False
-
-def checkBoxPartlyInsideArea(box, area):
-    if (checkInsideArea(area, [box[0], box[1]]) or
-        checkInsideArea(area, [box[0], box[3]]) or
-        checkInsideArea(area, [box[2], box[1]]) or
-        checkInsideArea(area, [box[2], box[3]])):
-        return True
-    else:
-        return False
+def checkOverlap(box, area):
+    if min(box[1], box[3]) > max(area[1], area[3]): return False
+    if max(box[1], box[3]) < min(area[1], area[3]): return False
+    if min(box[0], box[2]) > max(area[0], area[2]): return False
+    if max(box[0], box[2]) < min(area[0], area[2]): return False
+    return True
 
 print(box)
 
@@ -37,9 +26,7 @@ for line in f:
     if match1:
         # Note input is lat, lon, lat, lon
         area = [float(match1.group(i)) for i in range(1, 5)]
-        if checkBoxPartlyInsideArea(box, area):
-            #print(area)
-            #print(tile)
+        if checkOverlap(box, area):
             print('/usr/bin/time mkgmap --precomp-sea=./data/planet/sea.zip --bounds=./data/planet/bounds.zip --max-jobs --remove-ovm-work-files --output-dir=./data/garmin ./data/osm-tiles/%s.osm.pbf' % (tile))
             print('/usr/bin/time mono /opt/srtm2osm/Srtm2Osm.exe -d ./data/srtm -bounds1 %f %f %f %f -o ./data/osm-tiles/5%s.osm -step 10 -cat 400 100' % (area[0], area[1], area[2], area[3], tile[1:]))
             print('/usr/bin/time mkgmap --draw-priority=10000 --transparent --remove-ovm-work-files --output-dir=./data/garmin ./data/osm-tiles/5%s.osm' % (tile[1:]))
